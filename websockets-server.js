5027895348bdbcdace28c39b2d8cfd3500afd6d9
Justine Tran <jhtran@csu.fullerton.edu>
2018-03-16 00:55:12 -0700

    Added a /topic command to the chat server to track what users are discussing

--- websockets-server.js
+++ websockets-server.js
@@ -5,10 +5,17 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
+var topic;
 console.log("websockets server started");
 
 ws.on("connection", function(socket) {
   console.log("client connection established");
+
+  if (topic) { //if topics is not empty
+    var curTopic = "*** Topic is ";
+    curTopic += "'" + topic + "'";
+    socket.send(curTopic); //send current topic to new user first
+  }
 
   messages.forEach(function(msg) { //allow new users to see prev msgs
     socket.send(msg);
@@ -16,10 +23,20 @@ ws.on("connection", function(socket) {
 
   socket.on("message", function(data) { //echo server: repeat messages sent
     console.log("message received: " + data);
-    messages.push(data); //keep log of messages
-    ws.clients.forEach(function(clientSocket) { //send new msgs to all users
-      clientSocket.send(data);
-    });
+    if (data.indexOf("/topic") != -1) { //if message is /topic command
+      var changedTopic = "*** Topic has been changed to ";
+      changedTopic += "'" + data.substring(7, data.length) + "'";
+      topic = data.substring(7, data.length);
+      ws.clients.forEach(function(clientSocket) { //send new msgs to all users
+        clientSocket.send(changedTopic);
+      });
+    } else {
+      messages.push(data); //keep log of messages
+      ws.clients.forEach(function(clientSocket) { //send new msgs to all users
+        clientSocket.send(data);
+      });
+    }
+
     // socket.send(data);
   });
 });
